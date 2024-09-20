fa01a63605b32d65b17a43117bab362e14a94e98
wflixu <wflixu@88.com>
2024-09-20 11:59:18 +0800

    Initial Commit

---
+++ FinderSyncer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  FinderSyncer
+//
+//  Created by 李旭 on 2024/9/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ FinderSyncer/FinderSyncerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinderSyncerApp.swift
+//  FinderSyncer
+//
+//  Created by 李旭 on 2024/9/20.
+//
+
+import SwiftUI
+
+@main
+struct FinderSyncerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
