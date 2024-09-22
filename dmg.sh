818a2ee482d0a9d837ecf41d5aaa471ccd41141d
wflixu <wflixu@88.com>
2024-09-22 13:19:23 +0800

    build: add craete-dmg shell

---
+++ dmg.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+APP_NAME="FinderSyncer"
+DMG_FILE_NAME="Build/Products/Release/${APP_NAME}-Installer.dmg"
+VOLUME_NAME="${APP_NAME} Installer"
+SOURCE_FOLDER_PATH="Build/Products/Release/${APP_NAME}.app"
+
+
+test -f "${DMG_FILE_NAME}" && rm "${DMG_FILE_NAME}"
+
+# Create the DMG
+create-dmg \
+  --volname "${VOLUME_NAME}" \
+  --window-pos 200 120 \
+  --window-size 800 400 \
+  --icon-size 100 \
+  --icon "${APP_NAME}.app" 200 190 \
+  --hide-extension "${APP_NAME}.app" \
+  --app-drop-link 600 185 \
+  "${DMG_FILE_NAME}" \
+  "${SOURCE_FOLDER_PATH}"
